be6e006f4d9f624bd9dd5bc98af923aa438fa2ba
Michele Hindman <meechiru@gmail.com>
2015-03-31 14:21:59 -0500

    Update models.py

--- menu/models.py
+++ menu/models.py
@@ -42,7 +42,7 @@ class Order(models.Model):
 	#menu_items = models.ManyToManyField(MenuItem)
 	table_number = models.IntegerField()
 	modifications = models.TextField()
-	status = models.CharField(max_length=64, choices=status_choices, default='in-progress')
+	#status = models.CharField(max_length=64, choices=status_choices, default='in-progress')
 	total_price = models.DecimalField(max_digits=8, decimal_places=2)
 	timestamp_created = models.DateTimeField(auto_now_add=True)
 	def __unicode__(self):
