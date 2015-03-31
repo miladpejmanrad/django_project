17ccd9b01b999b9ef6dfc0c842f0a1a54a0c1462
Michele Hindman <meechiru@gmail.com>
2015-03-31 14:15:52 -0500

    Update models.py
    Made some additional changes to the Order and MenuItem classes. Also commented out the changes that I'm not 100% will work, since I haven't tested the code yet.

--- menu/models.py
+++ menu/models.py
@@ -11,25 +11,39 @@ class Category(models.Model):
 		verbose_name_plural = "categories"
 
 class MenuItem(models.Model):
+	#allergen_choices = (
+	#	('peanuts', 'Peanuts'),
+	#	('tree-nuts', 'Tree nuts'),
+	#	('milk', 'Milk'),
+	#	('eggs', 'Eggs'),
+	#	('gluten', 'Gluten'),
+	#	('soy', 'Soy'),
+	#	('fish', 'Fish'),
+	#	('shellfish', 'Shellfish')
+	#)
+	vegetarian = models.BooleanField(default=False)
 	category = models.ForeignKey(Category)
 	name = models.CharField(max_length=200)
 	description = models.TextField()
 	price = models.DecimalField(max_digits=8, decimal_places=2)
 	main_photo = models.ImageField(upload_to = 'menu/items/')
+	#allergens = models.CharField(max_length=64, choices=allergen_choices, default='none')
 	def __unicode__(self):
 		return self.name
 		
 class Order(models.Model):
-	status_choices = (
-	        ('in-progress', 'In Progress'),
-	        ('cooking', 'Cooking'),
-	        ('ready-to-serve', 'Ready to Serve'),
-	        ('served', 'Served'),
-	        ('paid', 'Paid')
-        )
-	menu_items = models.ManyToManyField(MenuItem)
+	#status_choices = (
+	#        ('in-progress', 'In Progress'),
+	#        ('cooking', 'Cooking'),
+	#        ('ready-to-serve', 'Ready to Serve'),
+	#        ('served', 'Served'),
+	#        ('paid', 'Paid')
+        #)
+	#menu_items = models.ManyToManyField(MenuItem)
 	table_number = models.IntegerField()
 	modifications = models.TextField()
 	status = models.CharField(max_length=64, choices=status_choices, default='in-progress')
 	total_price = models.DecimalField(max_digits=8, decimal_places=2)
 	timestamp_created = models.DateTimeField(auto_now_add=True)
+	def __unicode__(self):
+		return self.id
