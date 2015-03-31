b302dbd1691bc3c9bdb26d1a1508171dd185db3f
Michele Hindman <meechiru@gmail.com>
2015-03-31 13:33:03 -0500

    Update models.py
    Started developing the Order class.

--- menu/models.py
+++ menu/models.py
@@ -18,3 +18,18 @@ class MenuItem(models.Model):
 	main_photo = models.ImageField(upload_to = 'menu/items/')
 	def __unicode__(self):
 		return self.name
+		
+class Order(models.Model):
+	status_choices = (
+	        ('in-progress', 'In Progress'),
+	        ('cooking', 'Cooking'),
+	        ('ready-to-serve', 'Ready to Serve'),
+	        ('served', 'Served'),
+	        ('paid', 'Paid')
+        )
+	menu_items = models.ManyToManyField(MenuItem)
+	table_number = models.IntegerField()
+	modifications = models.TextField()
+	status = models.CharField(max_length=64, choices=status_choices, default='in-progress')
+	total_price = models.DecimalField(max_digits=8, decimal_places=2)
+	timestamp_created = models.DateTimeField(auto_now_add=True)
