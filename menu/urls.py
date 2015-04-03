9f798c5904646cf07cf269ad71d039370f892700
Michele Hindman <meechiru@gmail.com>
2015-04-03 14:15:47 -0500

    Moved menu template

--- menu/urls.py
+++ menu/urls.py
@@ -3,8 +3,7 @@ from menu import views
 
 urlpatterns = patterns('',
 	# /menu/
-	url(r'^$', views.index, name='index'),
-	url(r'^mainMenu$', views.mainMenu, name='mainMenu'),
+	url(r'^$', views.menu, name='menu'),
 	# /menu/category/7
 	url(r'^category/(?P<category_id>\d+)/$', views.categories, name='category')
 )
--- menu/views.py
+++ menu/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from menu.models import Category, MenuItem
 
 # Create your views here.
-def index(request):
+def menu(request):
 	categories_list = Category.objects.order_by('name')
 	context = {'categories_list': categories_list}
-	return render(request, 'menu/index.html', context)
+	return render(request, 'menu.html', context)
 
 def categories(request, category_id):
 	return HttpResponse("You're looking at category %s." % category_id)
