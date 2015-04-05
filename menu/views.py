b18eb79feeeb5fdd56f90c0c769e69523feb9c18
Michele Hindman <meechiru@gmail.com>
2015-04-04 20:35:00 -0500

    Review & place orders
    The Review Order button works now. Displays a list of the items in the
    order, the price, the modifications field, and a Place Order button.
    Submitting with the Place Order button updates the status of the order
    from "ordering" to "in progress."

--- menu/views.py
+++ menu/views.py
@@ -111,23 +111,34 @@ def add_to_order(request, menu_item_id):
 			
 	return HttpResponse("You're trying to order %s, but it didn't go through." % menu_item)
 	
-# This view shows the user their order and lets them submit to the kitchen
+# This view shows the user their order and lets them submit to the kitchen.
+# Submitting the order changes the status to "in-progress", which should be handled by the kitchen views.
 def review_order(request):
 
-	# Check to see if an order is already set for this table. If so, modify it. If not, create a new one.
-	existing_order = Order.objects.filter(table_number=settings.TABLE_NUMBER, status='ordering')
-		
+	# Check to see if an order is ready to be placed for this table.
+	order_to_send = Order.objects.filter(table_number=settings.TABLE_NUMBER, status='ordering')
+	
+	# If the order gets submitted, save it and redirect.
 	if request.method == "POST":
-		if existing_order.exists():
-			order_form = OrderForm(request.POST, instance=existing_order.get())
-		else:
-			order_form = OrderForm(request.POST)
+		order_form = OrderForm(request.POST, instance=order_to_send.get())
 		if order_form.is_valid():
 			order_form.save()
-			return HttpResponseRedirect("/menu/review-order")
+			return HttpResponseRedirect("/menu/review-order/")
+	
+	# Build the context and form for the template if an order is ready to be placed.
+	if order_to_send.exists():
+		ordered_items = list(order_to_send.get().menu_items.all()) # Get the menu items already on the order
+		order_form = OrderForm(
+			initial={
+				'status': 'in-progress'
+			}, instance=order_to_send.get())
+		
+		context = {
+			'order': order_to_send.get(),
+			'ordered_items': ordered_items,
+			'form': order_form
+		}
+		return render(request, 'review-order.html', context)
 			
-	if existing_order.exists():
-		return HttpResponse("There's an order here ready to send.")
-			
-	return HttpResponse("There isn't an order to review.")
+	return HttpResponse("Order sent! Need the template to handle the success message.")
     
