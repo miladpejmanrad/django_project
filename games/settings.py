c9bd03bb380bdb3d9e49a890fc3aa89e67c69933
Michele Hindman <meechiru@gmail.com>
2015-04-05 11:32:38 -0500

    Cleaned up games files, made Flappy Bird work
    Cleaned up the templates/views for the games, moved the Javascript file
    for Flappy Bird to /media/assets/js so it would load.

--- games/settings.py
+++ games/settings.py
@@ -1,6 +1,3 @@
 """
-Defines some global settings for the restaurant app.
+Defines some global settings for the games app.
 """
-
-# This would hypothetically be configured for each table the app is set up at
-TABLE_NUMBER = 1
--- games/views.py
+++ games/views.py
@@ -1,21 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
-from menu.models import Category, MenuItem, Order
-from menu.modelforms import OrderForm
-from menu import settings
+from django.shortcuts import render
 
 # This returns and sets up the contexts for the main menu.html template
 def games(request):
-	existing_order = Order.objects.filter(table_number=settings.TABLE_NUMBER, status='ordering')
-	categories_list = Category.objects.order_by('name')
-	context = {
-		'categories_list': categories_list,
-		'order_exists': existing_order.exists(),
-	}
-	return render(request, 'games.html', context)
+	return render(request, 'games.html')
 
 # This returns and sets up the contexts for the individual categories and their menu items for the menu.html template
 def flappybird(request):
-
 	return render(request, 'flappybird.html')
