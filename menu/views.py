dbbb7dfc5c65f778410e926d3001f41243b1bc06
Michele Hindman <meechiru@gmail.com>
2015-04-11 14:36:50 -0500

    Help button creates notifications
    Pressing the help button creates a notification in the system. Working
    on the other types now.

--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
-from menu.models import Category, MenuItem, Order, Allergen, AdminMenu
+from menu.models import Category, MenuItem, Order, Allergen, AdminMenu, Notification
 from menu.modelforms import AddItemToOrderForm, PlaceOrderForm, TipOrderForm
 from decimal import Decimal
 from binascii import a2b_base64
@@ -299,9 +299,12 @@ def receipt(request, receipt_type):
 	
 # This view handles the notifications sent by the customer.
 def send_notification(request):
+	context = {}
 	if request.method == "POST":
 		notification_type = request.POST['type']
-		drink_name = request.POST['drink']
+		drink_name = ''
+		if notification_type == 'refill':
+			drink_name = request.POST['drink']
 		new_notification = Notification(
 			table_number = settings.TABLE_NUMBER,
 			type = notification_type,
@@ -309,7 +312,7 @@ def send_notification(request):
 		)
 		new_notification.save()
 		context = {
-			'notification_type' = notification_type,
+			'notification_type': notification_type,
 		}
 		
-	return render(request, 'menu/notification.html', context)
+	return render(request, 'notification.html', context)
