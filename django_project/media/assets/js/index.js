8419fe27f70cc46677f415503909d3375fa0c478
JordanYankovich <j.yankovich@austinlane.com>
2015-04-13 15:58:40 -0700

    Fixed flappy bird
    updated flappy bird JavaScript logic to end game on failure, the bird is
    no longer invincible

--- django_project/media/assets/js/index.js
+++ django_project/media/assets/js/index.js
@@ -1,12 +1,12 @@
 $(function(){
-	$window = $('.window'),
-	$bird = $('.bird'),
+  $window = $('.window'),
+  $bird = $('.bird'),
   fallTime = 1000,
   gapHeight = 120,
   gameState = 2,
   pipeId = 0;
 
-	var int = setInterval(function(){
+  var int = setInterval(function(){
     if(gameState === 1){
       spawnPipe();
       movePipes();
@@ -19,86 +19,82 @@ $(function(){
     }
   }, 10);
 
-	$window.mousedown(function(){
-		birdFlap();
+  $window.mousedown(function(){
+    birdFlap();
     if(gameState === 2){
       gameState = 1;
       deleteInterval();
     }
-    if(gameState == 0){
-      gameState = 1 
-      setInterval(); 
-    }
-	});
+  });
   
-	$(window).keydown(function(e){
-		if(e.keyCode === 32){
-			birdFlap();
+  $(window).keydown(function(e){
+    if(e.keyCode === 32){
+      birdFlap();
       e.preventDefault();
       if(gameState === 2){
         gameState = 1;
         deleteInterval();
       }
-		}
-	});
+    }
+  });
 
   function deleteInterval(){
-	  setTimeout(function(){
-		  var int= setInterval(function(){ 
+    setTimeout(function(){
+      var int= setInterval(function(){ 
         if(gameState === 1){
           deletePipe();
         }
       }, 1300);
-	  }, 2050);
+    }, 2050);
   }
   
-	function birdFlap(){
+  function birdFlap(){
     if(gameState === 1 || gameState === 2){
       $bird.css('transform', 'rotate(-20deg)');
-		  $bird.stop().animate({
-			  bottom: '+=60px'
-  		}, 200, function(){
+      $bird.stop().animate({
+        bottom: '+=60px'
+      }, 200, function(){
         birdPos();
         $bird.css('transform', 'rotate(0deg)');
-		  	$bird.stop().animate({
-			    bottom: '-=60px'
-  		  }, 300, 'linear', function(){
+        $bird.stop().animate({
+          bottom: '-=60px'
+        }, 300, 'linear', function(){
           birdPos();
           gravity();
         });
-	  	});
+      });
     }
-	}
+  }
 
-	function gravity(){
+  function gravity(){
     birdPercent = parseInt($bird.css('bottom')) / $window.height();
     totalFallTime = fallTime * birdPercent;
-		$bird.stop().animate({
-			bottom: '0'
-		}, totalFallTime, 'linear');
+    $bird.stop().animate({
+      bottom: '0'
+    }, totalFallTime, 'linear');
 
     $bird.css('transform', 'rotate(90deg)');
-	}
+  }
 
-	function spawnPipe(){
+  function spawnPipe(){
     pipeId++;
-		pipeTopHeight = Math.floor(Math.random() * ($window.height() - 250)) + 50;
-		pipeBottomHeight = $window.height() - (pipeTopHeight + gapHeight);
-		pipe = '<div class="pipe" pipe-id="' + pipeId + '"><div style="height: ' + pipeTopHeight + 'px" class="topHalf"></div><div style="height:' + pipeBottomHeight + 'px" class="bottomHalf"></div></div>';
-		$window.append(pipe);
-	}
+    pipeTopHeight = Math.floor(Math.random() * ($window.height() - 250)) + 50;
+    pipeBottomHeight = $window.height() - (pipeTopHeight + gapHeight);
+    pipe = '<div class="pipe" pipe-id="' + pipeId + '"><div style="height: ' + pipeTopHeight + 'px" class="topHalf"></div><div style="height:' + pipeBottomHeight + 'px" class="bottomHalf"></div></div>';
+    $window.append(pipe);
+  }
 
-	function deletePipe(){
-		$('.window .pipe').first().remove();
-	}
+  function deletePipe(){
+    $('.window .pipe').first().remove();
+  }
 
-	function movePipes(){
-		$('.pipe').each(function(){
-			$(this).animate({
-				right: '+=160px'
-			}, 1300, 'linear');
-		});
-	}
+  function movePipes(){
+    $('.pipe').each(function(){
+      $(this).animate({
+        right: '+=160px'
+      }, 1300, 'linear');
+    });
+  }
   
   function birdPos(){
     if(parseInt($bird.css('bottom')) === 0){
@@ -121,10 +117,8 @@ $(function(){
   
   function gameEnd(){
     clearInterval(birdPosInterval);
-    setInterval();
-  	$('.pipe').stop();
+    $('.pipe').stop();
     gravity();
-  	gameState = 0;
-
+    gameState = 0;
   }
 })
